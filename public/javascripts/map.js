ae5bb8966c2f72c18b2206d809cdeb3f3f539c94
Javier Gorgoroso <pararegistrarme9@gmail.com>
2022-11-22 17:13:07 -0300

    Página principal

---
+++ public/javascripts/map.js
@@ -0,0 +1,14 @@
+var map = L.map('main_map').setView([-34.891086, -56.187019], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker1 = L.marker([-34.891086, -56.187019]).addTo(map);
+var marker2 = L.marker([-34.888445, -56.185814]).addTo(map);
+var marker3 = L.marker([-34.887266, -56.183782]).addTo(map);
+
+marker1.bindPopup("<b>palacio legislativo</b>").openPopup();
+marker2.bindPopup("<b>Facultad de química</b><br>edificio central").openPopup();
+marker3.bindPopup("<b>MAM</b><br>mercado agrícola montevideo").openPopup();

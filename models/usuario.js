710f98000be06bad1346290484dfd8cb349f54b3
Javier Gorgoroso <pararegistrarme9@gmail.com>
2022-12-16 20:56:50 -0300

    fix sendgrid

--- models/usuario.js
+++ models/usuario.js
@@ -72,7 +72,7 @@ usuarioSchema.methods.enviar_email_bienvenida = function(cb) {
         if (err) { return console.log(err.message);}
 
         const mailOptions = {
-            from: 'no-reply@redbicicletas.com',
+            from: 'pararegistrarme9@gmail.com',
             to: email_destination,
             subject: 'Verificación de cuenta',
             text: 'Hola, \n\n' + 'Por favor, para verificar su cuenta haga click en este link: \n' + 'http://localhost:3000' + '\/token/confirmation\/' + token.token + '\n',

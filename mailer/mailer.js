dc70eba9d0007f51cf937ecec7fb094f41eb09ca
Javier Gorgoroso <pararegistrarme9@gmail.com>
2022-12-16 20:25:07 -0300

    agregamos sendgrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,37 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'krystal83@ethereal.email',
-        pass: 'uNRDzhfhdvh3N5Sjc3'
+
+let mailConfig;
+if (process.env.NODE_ENV === 'production'){
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET
+        }
     }
-};
+    mailConfig = sgTransport(options);
+
+}else{
+    if (process.env.NODE_ENV === 'starting'){
+        console.log('XXXXXXXXXXXXXXXX');
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        }
+        mailConfig = sgTransport(options);
+
+    } else {
+        // all emails are catched by ethereal.email
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ethereal_user,
+                pass: process.env.ethereal_pwd
+            }
+        };
+    }
+}
 
 module.exports = nodemailer.createTransport(mailConfig);
